c07810628b284e63ee94f8e95103f197470ac8a8
phuc50centimeters <186391026+phuc50cm@users.noreply.github.com.>
2025-04-25 13:19:36 +0700

    Fix style issues

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"time"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -90,8 +90,8 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
 		ReadHeaderTimeout: 5 * time.Second,
 	}
 
